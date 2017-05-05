5657f0471b514ba4d4ffc10ec3398e2f2aa821ab
Corey Robison <corey@veteransadvantage.com>
2017-05-05 16:42:34 -0600

    trying to figure out sidebar

---
+++ shared/sidebar/sidebar.directive.js
@@ -0,0 +1,35 @@
+angular.module('app').directive('sidebarDir', function () {
+
+    return {
+        restrict: 'E',
+        templateUrl: 'shared/sidebar/sidebarView.html',
+        link: function (scope, element, atts) {
+
+            function myAccFunc() {
+                var x = document.getElementById("demoAcc");
+                if (x.className.indexOf("w3-show") == -1) {
+                    x.className += " w3-show";
+                    x.previousElementSibling.className += " w3-green";
+                } else {
+                    x.className = x.className.replace(" w3-show", "");
+                    x.previousElementSibling.className =
+                        x.previousElementSibling.className.replace(" w3-green", "");
+                }
+            }
+
+            function myDropFunc() {
+                var x = document.getElementById("demoDrop");
+                if (x.className.indexOf("w3-show") == -1) {
+                    x.className += " w3-show";
+                    x.previousElementSibling.className += " w3-green";
+                } else {
+                    x.className = x.className.replace(" w3-show", "");
+                    x.previousElementSibling.className =
+                        x.previousElementSibling.className.replace(" w3-green", "");
+                }
+            }
+
+
+        }
+    };
+});
